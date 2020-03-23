bda5e7a453e7ef431589c5ce49b624e35899b90f
Brayan Llanos <brayan0428@gmail.com>
2020-03-22 20:12:30 -0500

    Se configuro el angular material y se creo un modulo de administracion para la gestión de los productos

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -5,9 +5,10 @@ import { ProductComponent } from "./components/product/product.component";
 import { ProductDetailComponent } from "./components/product-detail/product-detail.component";
 import { SharedModule } from "../shared/shared.module";
 import { ProductRoutingModule } from "./product-routing.module";
+import { MaterialModule } from "../material/material.module";
 
 @NgModule({
   declarations: [ProductsComponent, ProductComponent, ProductDetailComponent],
-  imports: [CommonModule, SharedModule, ProductRoutingModule]
+  imports: [CommonModule, SharedModule, ProductRoutingModule, MaterialModule]
 })
 export class ProductModule {}
